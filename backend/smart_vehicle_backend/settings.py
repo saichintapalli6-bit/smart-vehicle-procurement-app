54fdfa7648eecb9487e2ea1f21bafd4816772af3
Sai <saichintapalli6@gmail.com>
2026-03-14 00:00:46 +0530

    Fix database client encoding error

--- backend/smart_vehicle_backend/settings.py
+++ backend/smart_vehicle_backend/settings.py
@@ -69,7 +69,10 @@ WSGI_APPLICATION = 'smart_vehicle_backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=0
+        conn_max_age=0,
+        conn_params={
+            'options': '-c client_encoding=utf8'
+        }
     )
 }
 
