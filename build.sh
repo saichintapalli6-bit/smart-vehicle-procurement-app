a0ec40a87e28961e327e3402d204d8daeec39c73
Sai <saichintapalli6@gmail.com>
2026-03-13 21:49:33 +0530

    Add build files to root for Render

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Go to backend directory
+cd backend
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
