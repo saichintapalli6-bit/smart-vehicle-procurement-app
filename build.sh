e821c9ec9be7b56f93d294ef024503df875187f1
Sai <saichintapalli6@gmail.com>
2026-04-11 16:10:09 +0530

    Fix Render build error: use virtual environment for dependencies

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 set -o errexit
 
 # Install dependencies from root
+python -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Go to backend directory for Django commands
 cd backend
 
 python manage.py collectstatic --no-input
-# python manage.py migrate  <-- Moved to Render Release Command / Pre-deploy
+# python manage.py migrate
